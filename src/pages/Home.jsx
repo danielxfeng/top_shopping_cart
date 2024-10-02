a11caca50519a050303c3449782f450d0858b67d
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-02 19:11:07 +0300

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 const Home = () => {
   return (
     <div>
-      <p>Home</p>
+      <h2>Welcome</h2>
+      <p>
+        We are thrilled to welcome you to our store, your one-stop destination
+        for all your shopping needs! Whether you're looking for the latest
+        electronics, stylish clothing, or unique gifts, we've got you covered
+        with a wide range of high-quality products at unbeatable prices.
+      </p>
+      <p>
+        At our store, we strive to provide an exceptional shopping experience
+        with an easy-to-navigate store, fast and secure checkout, and a team of
+        customer service experts ready to assist you with any questions.
+      </p>
+      <p>
+        Enjoy browsing through our carefully curated collections, and don’t
+        forget to check out the exclusive deals available in each category!
+      </p>
+      <p>Happy Shopping!</p>
     </div>
   );
 };
