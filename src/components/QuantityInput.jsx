c2505e0a82d38aea2fba389ec2dd477f51bb402f
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-03 23:29:18 +0300

    Styled QuantityInput component.

--- src/components/QuantityInput.jsx
+++ src/components/QuantityInput.jsx
@@ -1,47 +1,34 @@
 import { useRef } from "react";
+import styles from "../styles/QuantityInput.module.css";
 
 // This component is used to input quantity of a product
 const QuantityInput = ({ quantity, setQuantity }) => {
   const ref = useRef(null);
 
   // Add the quantity of the item.
-  const add = () => {
-    setQuantity(quantity + 1);
-    ref.current.classList.remove("show");
-  };
+  const add = () => setQuantity(quantity + 1);
 
   // Subtract the quantity of the item. Cannot be less than 0.
   const subtract = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    } else {
-      ref.current.classList.add("show");
-    }
+    if (quantity > 1) setQuantity(quantity - 1);
   };
 
   // Input the quantity of the item.
   const inputQuantity = (value) => {
     const number = parseInt(value, 10);
-    if (!isNaN(number) && number > 0) {
-      setQuantity(number);
-      ref.current.classList.remove("show");
-    } else {
-      ref.current.classList.add("show");
-    }
+    if (!isNaN(number) && number > 0) setQuantity(number);
   };
 
   return (
-    <div>
+    <div className={styles.qi}>
+      <button type="button" onClick={subtract}>
+        -
+      </button>
       <input
         type="number"
         value={quantity}
         onChange={(e) => inputQuantity(e.target.value)}
       />
-      <p ref={ref}>Please input a valid number that greater than 0.</p>
-      <button type="button" onClick={subtract}>
-        -
-      </button>
-      {quantity}
       <button type="button" onClick={add}>
         +
       </button>
