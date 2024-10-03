a5ace5d1b228ea19f14038002da10fb5e4f96943
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-10-03 22:31:50 +0300

    Styled Cart.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import cart from "../services/cart";
 import { useCartUpdateSign } from "../context/cartContext";
 import QuantityInput from "../components/QuantityInput";
+import styles from "../styles/Cart.module.css";
 
 // CartItem is a component that displays a single item in the shopping cart.
 const CartItem = ({ item }) => {
@@ -22,23 +23,23 @@ const CartItem = ({ item }) => {
 
   return (
     <tr>
-      <td>
-        <img src={item.product.image} alt={item.product.title} />
-        {item.product.title}
+      <td className={styles.product__description}>
+        <img className={styles.product__img} src={item.product.image} alt={item.product.title} />
+        <span>{item.product.title}</span>
       </td>
       <td>
-        <span>€</span>
+        <span className={styles.currency}>€</span>
         {item.product.price.toFixed(2)}
       </td>
       <td>
         <QuantityInput quantity={quantity} setQuantity={setQuantity} />
       </td>
       <td>
-        <span>€</span>
+        <span className={styles.currency}>€</span>
         {cart.getSubTotal(item).toFixed(2)}
       </td>
       <td>
-        <button type="button" onClick={() => remove()}>
+        <button className={styles.btn__remove} type="button" onClick={() => remove()}>
           Remove
         </button>
       </td>
@@ -53,6 +54,7 @@ const Cart = () => {
 
   // Checkout the cart.
   const checkout = async () => {
+    if (cart.getItemCount() === 0) return;
     try {
       const res = await cart.checkout();
       cart.clearCart();
@@ -66,51 +68,53 @@ const Cart = () => {
   return (
     <div>
       <h1>Shopping Cart</h1>
-      <section>
-        <table>
-          <caption>Check your shopping cart here:</caption>
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Subtotal</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.getItemCount() === 0 && (
+      <div className={styles.container}>
+        <section className={styles.cart}>
+          <table>
+            <caption>Check your shopping cart here:</caption>
+            <thead>
               <tr>
-                <td colSpan="3">Your cart is empty.</td>
+                <th className={styles.head__product}>Product</th>
+                <th className={styles.head__item}>Price</th>
+                <th className={styles.head__item}>Quantity</th>
+                <th className={styles.head__item}>Subtotal</th>
+                <th className={styles.head__item}>Actions</th>
               </tr>
-            )}
-            {cart.getCart().map((item) => (
-              <CartItem key={item.product.id} item={item} />
-            ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="3">Total:</td>
-              <td>
-                <span>€</span>
-                {cart.getTotal().toFixed(2)}
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </section>
-      <aside>
-        <h2>Cart Summary</h2>
-        <p>
-          <span>€</span>
-          {cart.getTotal().toFixed(2)}
-        </p>
-        <button type="button" onClick={() => checkout()}>
-          Checkout
-        </button>
-      </aside>
-      <div>
-        <p>{result}</p>
+            </thead>
+            <tbody>
+              {cart.getItemCount() === 0 && (
+                <tr>
+                  <td className={"error"} colSpan="3">Your cart is empty.</td>
+                </tr>
+              )}
+              {cart.getCart().map((item) => (
+                <CartItem key={item.product.id} item={item} />
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan="3">Total:</td>
+                <td>
+                  <span className={styles.currency}>€</span>
+                  {cart.getTotal().toFixed(2)}
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </section>
+        <aside className={styles.summary}>
+          <h2>Summary</h2>
+          <p>
+            <span className={styles.currency}>Total €</span>
+            {cart.getTotal().toFixed(2)}
+          </p>
+          <button type="button" onClick={() => checkout()}>
+            Checkout
+          </button>
+          <div>
+            <p className="error">{result}</p>
+          </div>
+        </aside>
       </div>
     </div>
   );
